c037a8cb852a08a6a6a8b1fca33b169a56c3f63c
张振峰 <xyls_ok@126.com>
2017-06-09 16:48:04 +0800

    init

---
+++ src/main/java/com/xyls/wwyz/Main.java
@@ -0,0 +1,21 @@
+package com.xyls.wwyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+// import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+/**
+ * Created by gddev3 on 16/5/26.
+ */
+@SpringBootApplication
+@EnableConfigurationProperties()
+// @EnableFeignClients
+// @EnableEurekaClient
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
---
+++ src/main/java/com/xyls/wwyz/dao/JooqDao.java
@@ -0,0 +1,46 @@
+package net.gddata.organ.dao;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jooq.Configuration;
+import org.jooq.DSLContext;
+import org.jooq.Table;
+import org.jooq.UpdatableRecord;
+import org.jooq.impl.DAOImpl;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+
+@Slf4j
+public abstract class JooqDao<R extends UpdatableRecord<R>, P, T> extends DAOImpl<R, P, T> {
+
+    private DSLContext dslContext;
+
+    protected JooqDao(Table<R> table, Class<P> type) {
+        super(table, type);
+    }
+
+    @Resource
+    public void init(Configuration configuration) {
+        setConfiguration(configuration);
+    }
+
+    @Resource
+    public void initDslContext(DSLContext dslContext) {
+        this.dslContext = dslContext;
+    }
+
+    @PostConstruct
+    public void init() {
+        log.info("init {}", getClass().getName());
+    }
+
+    protected DSLContext create() {
+        return dslContext;
+    }
+
+    public void save(P p) {
+        insert(p);
+    }
+
+
+}
